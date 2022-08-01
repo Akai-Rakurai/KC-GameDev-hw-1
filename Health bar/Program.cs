287bf76c8b7579219bbc3718aa11682a92ffdb97
Akai-Rakurai <Dryusufhacks@gmail.com>
2022-08-01 14:36:43 +0300

    Added Homework

---
+++ Health bar/Program.cs
@@ -0,0 +1,15 @@
+﻿string hero = ("alex");
+int health = 100;
+Console.WriteLine("initial health: " + health);
+int Damage = (health - 20);
+Console.WriteLine("Health after damage is: " + Damage);
+int Boost = (Damage + 10);
+Console.WriteLine("Health after boost is: " + Boost);
+if (Damage > Boost)
+{
+    Console.WriteLine("You died");
+}
+else
+{
+    Console.WriteLine(Damage + " is less than " + Boost);
+}
